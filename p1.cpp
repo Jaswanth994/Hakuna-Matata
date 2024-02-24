264d88c76ac29f7a65968f288f5f7630e93c6b8d
Bunny256 <bunny.g2525@gmail.com>
2024-02-24 22:13:46 +0530

    Added run function and modified the code

--- p1.cpp
+++ p1.cpp
@@ -14,7 +14,7 @@ class core
 public:
     int registers[32];
     int pc;
-    void execute(string memory[]);
+    void execute(string memory);
 
     core()
     {
@@ -25,23 +25,61 @@ public:
         pc = 0;
     }
 };
-
-void core::execute(string memory[])
+class processor
 {
-    processor p;
-    // core c;
-    int n = memory->size();
-    int j = 0;
-    while (n-- > 0)
+public:
+    string memory[4096];
+    int clock;
+    processor()
     {
+        for (int i = 0; i < 4096; i++)
+        {
+            memory[i] = "";
+        }
+        clock = 0;
+    }
+    core cores[2] = {core(), core()};
+    
+    void run(string memory[]);
+    void load_Program(string filename, int start_addr);
+    void print();
+};
+void print1(processor p)
+{
+    cout << "[ ";
+    for (int i = 0; i < 32; i++)
+    {
+        cout << p.cores[0].registers[i] << " ";
+    }
+    cout << "]" << endl;
+}
+void print2(processor p)
+{
+    cout << "[ ";
+    for (int i = 0; i < 32; i++)
+    {
+        cout << p.cores[1].registers[i] << " ";
+    }
+   cout << "]" << endl;
+}
+
+void core::execute(string memory)
+{   
+    int i;
+    processor p;
+    int j=0;
         string t;
-        stringstream s1(memory[j]);
+        stringstream s1(memory);
         queue<string> s2;
         while (getline(s1, t, ' '))
         {
             s2.push(t);
         }
         int rd, r1, r2;
+        if(j<2048){
+            i=0;
+        }
+        else i=1;
         if (s2.front() == "add")
         {
             s2.pop();
@@ -50,10 +88,10 @@ void core::execute(string memory[])
             int r1 = stoi(s2.front().substr(1));
             s2.pop();
             int r2 = stoi(s2.front().substr(1));
-            // p.cores[0].registers[r1] = 7;
-            // p.cores[0].registers[r2] = 8;
-            p.cores[0].registers[rd] = p.cores[0].registers[r1] + p.cores[0].registers[r2];
-            p.cores[0].pc++;
+            registers[r1] = 7;
+            registers[r2] = 8;
+            registers[rd] = registers[r1] + registers[r2];
+            p.cores[i].pc++;
         }
         if (s2.front() == "sub")
         {
@@ -63,16 +101,16 @@ void core::execute(string memory[])
             int r1 = stoi(s2.front().substr(1));
             s2.pop();
             int r2 = stoi(s2.front().substr(1));
-            p.cores[0].registers[r1] = 26;
-            p.cores[0].registers[r2] = 9;
-            p.cores[0].registers[rd] = p.cores[0].registers[r1] - p.cores[0].registers[r2];
-            p.cores[0].pc++;
+            registers[r1] = 26;
+            registers[r2] = 9;
+            registers[rd] = registers[r1] - registers[r2];
+            pc++;
         }
         if (s2.front() == "lw")
         {
             int y;
             s2.pop();
-            if (!isdigit(s2.front()[2]))
+            /*if (!isdigit(s2.front()[2]))
             {
                 rd = int(s2.front()[1] - '0');
             }
@@ -90,12 +128,12 @@ void core::execute(string memory[])
                     r1 = int((s2.front()[1] - '0') * 10 + (s2.front()[2] - '0'));
                 }
                 s2.pop();
-                p.cores[0].registers[r1] = 1000;
-                p.memory[p.cores[0].registers[r1]] = "72";
-                y = stoi(p.memory[p.cores[0].registers[r1]]);
-                p.cores[0].registers[rd] = y;
-            }
-            else if (isdigit(s2.front()[0]))
+                registers[r1] = 1000;
+                p.memory[registers[r1]] = "72";
+                y = stoi(p.memory[registers[r1]]);
+                registers[rd] = y;
+            }*/
+            if(isdigit(s2.front()[0]))
             {
                 int z = 0;
                 int x = 0, r1;
@@ -115,12 +153,12 @@ void core::execute(string memory[])
                 }
                 else
                     r1 = int((s2.front()[z] - '0') + x);
-                p.cores[0].registers[r1] = 1000;
-                p.memory[p.cores[0].registers[r1]] = "72";
-                y = stoi(p.memory[p.cores[0].registers[r1]]);
-                p.cores[0].registers[rd] = y;
+                registers[r1] = 1000;
+                p.memory[registers[r1]] = "72";
+                y = stoi(p.memory[registers[r1]]);
+                registers[rd] = y;
             }
-            p.cores[0].pc++;
+            p.cores[i].pc++;
         }
         if (s2.front() == "addi")
         {
@@ -130,10 +168,10 @@ void core::execute(string memory[])
             int r1 = stoi(s2.front().substr(1));
             s2.pop();
             int value = stoi(s2.front().substr(0));
-            p.cores[0].registers[r1] = 6;
-            p.cores[0].registers[rd] = p.cores[0].registers[r1] + value;
+            registers[r1] = 6;
+            registers[rd] =registers[r1] + value;
             // cout<< p.c[i].registers[rd]<<endl;
-            p.cores[0].pc++;
+            p.cores[i].pc++;
         }
         if (s2.front() == "bne")
         {
@@ -145,14 +183,14 @@ void core::execute(string memory[])
             if (rd != r1)
             {
             }
-            p.cores[0].pc++;
+            p.cores[i].pc++;
         }
-        j++;
-    }
-    // cout<<p.cores[0].pc;
-    print1(p);
 }
-class processor
+
+    
+
+
+/*class processor
 {
 public:
     string memory[4096];
@@ -169,17 +207,16 @@ public:
     
     void run();
     void load_Program(string filename, int start_addr);
-};
+};*/
 
 void processor::load_Program(string filename, int start_addr) {
     ifstream file(filename);
     string line;
-
     int i = start_addr;
     if (file.is_open()) {
-        while (getline(file, line) && i < 2048+start_addr) {
-            if(line!="")
-                memory[i++] = line;
+        while (getline(file, line) && i < 2048+start_addr) {  
+                memory[i] = line;
+                i++;
         }
     }
     if(clock<i)
@@ -187,43 +224,48 @@ void processor::load_Program(string filename, int start_addr) {
     file.close();
 }
 
-void processor::run(){
-    
-    
-
-
+void processor::run(string m[]){
+int j=0; 
+     while(( j<2048 && memory[j]!="" && memory[j+2048]!="") || (j>=2048 && memory[j]!="" && memory[j-2048]!="")){
+        
+        if(m[j]!="" && j<2048){
+            cores[0].execute(m[j]);
+        }
+       else if(m[j]!="" && j>=2048){
+            cores[1].execute(m[j]);
+        }
+         if(j<2048){
+                j=j+2047;
+            }
+            else j=j-2048;
+            j++;
+            }
+        /*for(int k=0;k<3;k++){
+            cores[0].execute(m[k]);
+        }*/
 }
 
 
-
-void print1(processor p)
-{
-    cout << "[ ";
-    for (int i = 0; i < 32; i++)
-    {
-        cout << p.cores[0].registers[i] << " ";
-    }
-    cout << "]" << endl;
-}
-void print2(processor p)
-{
-    cout << "[ ";
-    for (int i = 0; i < 32; i++)
-    {
-        cout << p.cores[1].registers[i] << " ";
-    }
-    cout << "]" << endl;
-}
-
-void processor::run()
-{
-}
 
 int main()
 {
     processor sim;
-
+    core c;
     sim.load_Program("a.txt", 0);
     sim.load_Program("b.txt", 2048);
-
+    sim.run(sim.memory);
+    cout<<"[";
+    for(int i=0;i<32;i++){
+        cout<<sim.cores[0].registers[i]<<" ";
+    }
+    cout<<"]";
+    cout<<endl;
+    cout<<"[";
+    for(int i=0;i<32;i++){
+        cout<<sim.cores[1].registers[i]<<" ";
+    }
+    cout<<"]";
+    cout<<endl;
+    
+    //sim.print();
 }
