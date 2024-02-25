22e748fcbdfb33663273e37f698671ae663de963
G_Jaswanth <jaswanthgosu0909@gmail.com>
2024-02-25 19:07:18 +0530

    added li la and sorting functions

--- p1.cpp
+++ p1.cpp
@@ -43,6 +43,7 @@ public:
 
     void run();
     void load_Program(string filename, int start_addr);
+    void load_data(string memory[], int start_addr);
 };
 
 void core::execute(string instruction, string memory[])
@@ -55,7 +56,7 @@ void core::execute(string instruction, string memory[])
         s2.push(word);
     }
     int rd, r1, r2;
-    std::cout << s2.front() << " /////////" << endl;
+   // std::cout << s2.front() << " /////////" << endl;
     if (s2.front() == "add")
     {
         s2.pop();
@@ -90,7 +91,7 @@ void core::execute(string instruction, string memory[])
         registers[rd] = registers[r1] + value;
         pc++;
     }
-    
+
     else if (s2.front() == "bne")
     {
         s2.pop();
@@ -101,22 +102,39 @@ void core::execute(string instruction, string memory[])
         if (registers[rd] != registers[r1])
         {
             string label = s2.front();
+          //  cout<<label<<" NNN"<<endl;
+            pc = label_addr[label];
+           // cout<<memory[pc]<<endl;
+            return;
+        }
+        pc++;
+    }
+    else if(s2.front() == "blt")
+    {
+        s2.pop();
+        int rd = stoi(s2.front().substr(1));
+        s2.pop();
+        int r1 = stoi(s2.front().substr(1));
+        s2.pop();
+        if (registers[rd] < registers[r1])
+        {
+            string label = s2.front();
             pc = label_addr[label];
             return;
         }
         pc++;
     }
     else if (s2.front() == "lw")
-    {           
-            int y;
-            s2.pop();
-            if (!isdigit(s2.front()[2]))
-            {
-                rd = int(s2.front()[1] - '0');
-            }
-            else
-                rd = int((s2.front()[1] - '0') * 10 + s2.front()[2] - '0');
-            s2.pop();
+    {
+        int y;
+        s2.pop();
+        if (!isdigit(s2.front()[2]))
+        {
+            rd = int(s2.front()[1] - '0');
+        }
+        else
+            rd = int((s2.front()[1] - '0') * 10 + s2.front()[2] - '0');
+        s2.pop();
         if (isdigit(s2.front()[0]))
         {
             int z = 0;
@@ -133,34 +151,37 @@ void core::execute(string instruction, string memory[])
             }
             if (isdigit(s2.front()[z + 1]))
             {
-                r1 = int((s2.front()[z] - '0') * 10 + (s2.front()[z+1] - '0'));
+                r1 = int((s2.front()[z] - '0') * 10 + (s2.front()[z + 1] - '0'));
             }
             else
-            r1 = int((s2.front()[z] - '0'));
-            std::cout<<memory[1004]<<endl;
-            std::cout<<r1<<endl;
-            int i = registers[r1]+x;
-            std::cout<<i;
-            if(memory[i]!=""){
-            registers[rd]=stoi(memory[i]);}
-            else registers[rd]=0;
+                r1 = int((s2.front()[z] - '0'));
+           // std::cout << memory[1004] << endl;
+           // std::cout << r1 << endl;
+            int i = registers[r1] + x;
+           // std::cout << i;
+            if (memory[i] != "")
+            {
+                registers[rd] = stoi(memory[i]);
+            }
+            
         }
         pc++;
     }
-     else if(s2.front()=="sw"){
-            int y;
-            s2.pop();
-            if (!isdigit(s2.front()[2]))
-            {
-                r1 = int(s2.front()[1] - '0');
-            }
-            else
-                r1 = int((s2.front()[1] - '0') * 10 + s2.front()[2] - '0');
-            s2.pop();
-            if (isdigit(s2.front()[0]))
+    else if (s2.front() == "sw")
+    {
+        int y,x;
+        s2.pop();
+        if (!isdigit(s2.front()[2]))
+        {
+            r1 = int(s2.front()[1] - '0');
+        }
+        else
+            r1 = int((s2.front()[1] - '0') * 10 + s2.front()[2] - '0');
+        s2.pop();
+        if (isdigit(s2.front()[0]))
         {
             int z = 0;
-            int x = 0;
+            x = 0;
             if (isdigit(s2.front()[1]))
             {
                 x = int((s2.front()[0] - '0') * 10 + (s2.front()[1] - '0'));
@@ -173,12 +194,13 @@ void core::execute(string instruction, string memory[])
             }
             if (isdigit(s2.front()[z + 1]))
             {
-                rd = int((s2.front()[z] - '0') * 10 + (s2.front()[z+1] - '0'));
+                rd = int((s2.front()[z] - '0') * 10 + (s2.front()[z + 1] - '0'));
             }
             else
                 rd = int((s2.front()[z] - '0'));
-                memory[registers[rd]+x]=to_string(registers[r1]);
+            memory[registers[rd] + x] = to_string(registers[r1]);
         }
+       // cout<<memory[registers[rd] + x]<< " rrrrrrrrr "<<endl;
         pc++;
     }
     else if (s2.front() == "beq")
@@ -202,19 +224,36 @@ void core::execute(string instruction, string memory[])
         s2.pop();
         string label = s2.front();
         pc = label_addr[label];
+     //   cout<<pc<<" e"<<endl;
         return;
     }
     else if (s2.front() == "jal")
     {
         s2.pop();
-        string str=s2.front();
+        string str = s2.front();
         int rd = stoi(s2.front().substr(1));
         s2.pop();
         registers[rd] = pc + 1;
-        label_addr.insert(make_pair(str,registers[rd]));
+        label_addr.insert(make_pair(str, registers[rd]));
         string label = s2.front();
         pc = label_addr[label];
         return;
+    }
+    else if(s2.front()=="la"){
+        s2.pop();
+        int rd = stoi(s2.front().substr(1));
+        s2.pop();
+        string label = s2.front();
+        registers[rd]= label_addr[label];
+        pc++;
+    }
+    else if(s2.front()=="li"){
+        s2.pop();
+        int rd = stoi(s2.front().substr(1));
+        s2.pop();
+        int value = stoi(s2.front().substr(0));
+        registers[rd] = value;
+        pc++;
     }
     else
     {
@@ -229,13 +268,18 @@ void core::labels(int start_addr, string memory[])
         stringstream ss(memory[i]);
         string firstword;
         ss >> firstword;
-        // std::cout<<"hiiii"<<"    firstword"<<endl;
         if (!firstword.empty() && firstword.back() == ':')
         {
-            // std::cout << firstword << endl;
             firstword = firstword.substr(0, firstword.size() - 1);
             // std::cout << firstword << " 99999999   "<<i<<endl;
-            label_addr.insert(make_pair(firstword, i + 1));
+            auto it=label_addr.find(firstword);
+            if(it != label_addr.end()){
+                it->second=i+1;
+              //  cout<<it->second<<" ffffff "<<endl;
+            }
+            else
+                label_addr.insert(make_pair(firstword, i + 1));
+
         }
     }
 }
@@ -259,11 +303,58 @@ void processor::load_Program(string filename, int start_addr)
     file.close();
 }
 
+void processor::load_data(string memory[], int start_addr)
+{
+    int i = 1 + start_addr;
+    int j = 1001 + start_addr;
+    stringstream ss(memory[i]);
+    string firstword, secondword;
+    ss >> firstword;
+    ss >> secondword;
+    while (firstword != ".text")
+    {
+        if (secondword == ".word")
+        {
+            stringstream ss(memory[i]);
+            string token;
+            while (ss >> token)
+            {
+                if (token != ".word")
+                {
+                    memory[j] = token;
+                   // cout << memory[j] << endl;
+                    j++;
+                }
+            }
+        }
+        else if (secondword == ".string")
+        {
+            stringstream ss(memory[i]);
+            string firstword;
+            ss >> firstword;
+            memory[j] = firstword;
+            //cout << memory[j] << endl;
+            j++;
+
+            string str = memory[i].substr(memory[i].find_first_of('"') + 1, memory[i].find_last_of('"') - memory[i].find_first_of('"') - 1);
+
+            memory[j] = str;
+           // cout << memory[j] << endl;
+            j++;
+        }
+        i++;
+        stringstream ss(memory[i]);
+        ss >> firstword;
+        ss >> secondword;
+    }
+}
+
 void processor::run()
 {
     cores[0].labels(0, memory);
     cores[1].labels(2048, memory);
     int flag = 1;
+    cores[1].pc=2048;
     while (clock < 2048 && flag == 1)
     {
         flag = 0;
@@ -274,9 +365,10 @@ void processor::run()
                 cores[0].execute(memory[cores[0].pc], memory);
                 flag = 1;
             }
-            else if (memory[cores[1].pc + 2048] != "" && i == 1)
+            else if (memory[cores[1].pc ] != "" && i == 1)
             {
-                cores[1].execute(memory[cores[1].pc + 2048], memory);
+               // cout<<memory[cores[1].pc]<<endl;
+                cores[1].execute(memory[cores[1].pc ], memory);
                 flag = 1;
             }
         }
@@ -287,15 +379,17 @@ void processor::run()
 int main()
 {
     processor sim;
-    sim.load_Program("a.txt", 0);
-    sim.load_Program("b.txt", 2048);
-    sim.cores[0].registers[1] = 3;
-    sim.cores[0].registers[2] = 1;
-    sim.cores[1].registers[10] = 72;
-    sim.cores[1].registers[11] = 2;
-    sim.cores[0].registers[27] = 2000;
-    sim.memory[2000] = "333";
+    sim.load_Program("a1.txt", 0);
+    sim.load_Program("b1.txt", 2048);
+    sim.load_data(sim.memory, 0);
+    sim.load_data(sim.memory, 2048);
     sim.run();
+    for(int i=1001;i<1015;i++){
+        cout<<sim.memory[i]<<endl;
+    }
+    for(int i=3049;i<3070;i++){
+        cout<<sim.memory[i]<<endl;
+    }
     std::cout << "[";
     for (int i = 0; i < 32; i++)
     {
@@ -310,6 +404,4 @@ int main()
     }
     std::cout << "]";
     std::cout << endl;
-
-    // sim.print();
 }
